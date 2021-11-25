3234476b1a676ea500a98f8de695feedaa6ea1af
K <kaisontoro@gmail.com>
2021-11-24 19:11:18 -0700

    created grid

---
+++ script.js
@@ -0,0 +1,11 @@
+const gridBody = document.getElementById("gridBody");
+for (i = 0; i < 17; i++) {
+    const gridRow = document.createElement('div');
+    gridRow.classList.add("gridRow")
+    for (n = 0; n < 17; n++) {
+        const gridSquare = document.createElement('div');
+        gridSquare.classList.add('gridSquare');
+        gridRow.appendChild(gridSquare);
+    }
+    gridBody.appendChild(gridRow);
+}
