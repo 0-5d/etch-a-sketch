327623efaef242046dedccc421b85ff83d1e0aeb
K <kaisontoro@gmail.com>
2021-11-26 22:26:31 -0700

    tried my best

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const gridBody = document.getElementById("gridBody");
-for (i = 0; i < 17; i++) {
+for (i = 0; i < 16; i++) {
     const gridRow = document.createElement('div');
     gridRow.classList.add("gridRow")
-    for (n = 0; n < 17; n++) {
+    for (n = 0; n < 16; n++) {
         const gridSquare = document.createElement('div');
         gridSquare.addEventListener('mouseover', function () {
             gridSquare.classList.add('gridSquareDark')
@@ -20,6 +20,32 @@ function clear() {
         clearable[i].classList.add('gridSquare');
     }
 }
-
+/*
+function gridChange() {
+    const gridSize = prompt('desired grid size');
+    document.querySelectorAll('.gridRow').forEach(e => e.remove())
+    switch(true) {
+        case isNaN(gridSize):
+            break;
+        case gridSize < 0:
+            break;
+        case gridSize > 100:
+            break;
+    }
+    for (i = 0; i < gridSize + 1; i++) {
+        const gridRow = document.createElement('div');
+        gridRow.classList.add("gridRow");
+        for (n = 0; n < gridSize + 1; n++) {
+            const gridSquare = document.createElement('div');
+            gridSquare.addEventListener('mouseover', function () {
+                gridSquare.classList.add('gridSquareDark');
+            });
+            gridSquare.classList.add('gridSquare');
+            gridRow.appendChild(gridSquare);
+        }
+        gridBody.appendChild(gridRow);
+    }
+}
+*/
 const clearButton = document.getElementById('clearButton');
 clearButton.addEventListener('click', clear);
